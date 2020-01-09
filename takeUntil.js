e514c809ccb9b6c8e1a8aef0217a7e6f08c85384
QuintonEL <quintonlaborde@gmail.com>
2020-01-09 22:04:38 +0000

    function to take elements from an array until the callback returns a truthy value

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+const assertArraysEqual = function(first, second) {
+  let status = eqArrays(first, second);
+  if (status) {
+    console.log('✔️ ✔️ ✔️' + `  Assertion Passed: ${first} === ${second}`);
+  } else {
+    console.log('🔴 🔴 🔴' + `  Assertion Failed: ${first} !=== ${second}`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  // check that the arrays have matching length
+  if (first.length !== second.length) {
+    return false;
+  } else {
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let count of array) {
+    // check if callback is false
+    if (!callback(count)) {
+      result.push(count);
+    } else {
+      return result;
+    }
+  }
+  return result;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+// test cases
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
