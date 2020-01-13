5840fc477be26554f84aea651578b551527ab589
QuintonEL <quintonlaborde@gmail.com>
2020-01-13 22:37:46 +0000

    refactor with chai and mocha

--- tail.js
+++ tail.js
@@ -1,10 +1,12 @@
+// returns array without the first entry
 const tail = function(array) {
   let rest = [];
   if (array.length <= 1) {
     return rest;
+  } else {
+    rest = array.slice(1);
+    return rest;
   }
-  rest = array.slice(1);
-  return rest;
 };
 
 module.exports = tail;
