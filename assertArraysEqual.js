2719a93ba488df0e4f5c53e794bb6067c65a4521
QuintonEL <quintonlaborde@gmail.com>
2020-01-13 22:35:37 +0000

    refeactor with mocha and chai

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,11 +2,7 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(first, second) {
   let status = eqArrays(first, second);
-  if (status) {
-    console.log('✔️ ✔️ ✔️' + '   Assertion Passed');
-  } else {
-    console.log('🔴 🔴 🔴' + '   Assertion Failed');
-  }
+  return status;
 };
 
 module.exports = assertArraysEqual;
