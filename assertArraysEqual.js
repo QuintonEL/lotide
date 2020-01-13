c316b4256370e0fe72686f99956173730e0d6aad
QuintonEL <quintonlaborde@gmail.com>
2020-01-13 20:36:17 +0000

    added export and require for testing

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,12 @@
-const eqArrays = function(first, second) {
-  // check that the arrays have matching length
-  if (first.length !== second.length) {
-    return false;
-  }
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(first, second) {
   let status = eqArrays(first, second);
   if (status) {
-    console.log('✔️ ✔️ ✔️' + 'Assertion Passed');
+    console.log('✔️ ✔️ ✔️' + '   Assertion Passed');
   } else {
-    console.log('🔴 🔴 🔴' + 'Assertion Failed');
+    console.log('🔴 🔴 🔴' + '   Assertion Failed');
   }
 };
 
-assertArraysEqual([1,2,3],[1,2,3]);
+module.exports = assertArraysEqual;
