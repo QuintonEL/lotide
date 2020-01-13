cc7f7628e7ceed3343884a4e9b12fcb46a6f388d
QuintonEL <quintonlaborde@gmail.com>
2020-01-13 22:36:36 +0000

    refactored with mocha and chai

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✔️ ✔️ ✔️' + `   Assertion Passed: ${actual} === ${expected}`);
+    return true;
   } else {
-    console.log('🔴 🔴 🔴' + `   Assertion Failed: ${actual} !== ${expected}`);
+    return false;  
   }
-
 };
 
 module.exports = assertEqual;
