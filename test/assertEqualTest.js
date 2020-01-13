9eb0f0dacef841454095e8f2dd80f1b1b5f6444c
QuintonEL <quintonlaborde@gmail.com>
2020-01-13 22:39:25 +0000

    refactored with mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,12 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual(2, 3);
+describe("#assertEqual", () => {
+  it("returns true for hello and hello", () => {
+    assert.strictEqual(assertEqual('hello', 'hello'), true);
+  });
+
+  it("returns false for 2 and 3", () => {
+    assert.strictEqual(assertEqual(1, 2), false); 
+  });
+});
