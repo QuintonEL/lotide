cf11e8d05fff2a78f74addd123dc5913e60aeb4e
QuintonEL <quintonlaborde@gmail.com>
2020-01-14 02:42:49 +0000

    final touches

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,6 @@
 const eqArrays = require('./eqArrays');
 
+// return the boolean result from the eqArrays function
 const assertArraysEqual = function(first, second) {
   let status = eqArrays(first, second);
   return status;
